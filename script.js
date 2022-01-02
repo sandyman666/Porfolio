c67bd076872172a64bbecba55417f1af8ac1c54d
sandyman666 <sandeepan.majumdar@rwth-aachen.de>
2022-01-02 16:08:52 +0100

    cleaned up the layout a bit

--- script.js
+++ script.js
@@ -1,42 +1,70 @@
-// Graphing Sine Wave (Graphing Wave)
-// The Nature of Code
-// The Coding Train / Daniel Shiffman
-// https://youtu.be/JLAc9hMtcxk
-// https://thecodingtrain.com/learning/nature-of-code/3.6-graphing-sine.html
-
-// Sine Wave Graph Exercise: https://editor.p5js.org/codingtrain/sketches/EIbEYLTaZ
-// Playing With Period: https://editor.p5js.org/codingtrain/sketches/SbRC-G0lU
-// Graphing Wave: https://editor.p5js.org/codingtrain/sketches/c_S9jiXz-
-// Circular Wave Exercise: https://editor.p5js.org/codingtrain/sketches/mOm2Is7ba
 
 let angles = [];
-let angleV = [];
-let r = 4;
+let r = 0.5;
+let Const_frameRate=60;
+var speed_slider;
+
 
 function setup() {
   createCanvas(600, 400);
-  let total = floor(width / (r * 2));
+  frameRate( Const_frameRate )
+  speed_slider = createSlider(0, 0.8, 0.3, .01 );
+  speed_slider.position(700, 400);
+  col = speed_slider.value();
+  
+  let total = Const_frameRate*50; 
   for (let i = 0; i < total + 1; i++) {
-    angles[i] = map(i, 0, total, 0, 2 * TWO_PI);
-    // angleV[i] = 0.01 + i / 100;
+    angles[i] = map(i, 0, total, 0, 2 * TWO_PI*col);
+    
   }
+  
 }
 
 function draw() {
-  background(0);
+  background(127);
   translate(300, 200);
-  fill(252, 238, 33);
-  stroke(252, 238, 33);
+  
+  col = speed_slider.value();
+  
+  fill(255, 0, 0);
+  stroke(255, 0, 0);
+  
   beginShape();
+  
   for (let i = 0; i < angles.length; i++) {
     let y = map(sin(angles[i]), -1, 1, -200, 200);
-    strokeWeight(4);
+    strokeWeight(3);
     let x = map(i, 0, angles.length, -300, 300);
-    // line(x, 0, x, y);
     circle(x, y, r * 2);
-    // vertex(x,y);
-    angles[i] += 0.02;
-    // angles[i] += angleV[i];
+    angles[i] -= col;
   }
   endShape();
+  
+  fill(0, 255, 0);
+  stroke(0, 255, 0);
+  
+  beginShape();
+  for (let i = 0; i < angles.length; i++) {
+    let y = map(sin(angles[i]-2*PI/3), -1, 1, -200, 200);
+    strokeWeight(3);
+    let x = map(i, 0, angles.length, -300, 300);
+    circle(x, y, r * 2);
+    angles[i] -= col;
+  }
+  endShape();
+  
+  fill(0, 0, 255);
+  stroke(0, 0, 255);
+  
+  beginShape();
+  for (let i = 0; i < angles.length; i++) {
+    let y = map(sin(angles[i]-4*PI/3), -1, 1, -200, 200);
+    strokeWeight(3);
+    let x = map(i, 0, angles.length, -300, 300);
+    circle(x, y, r * 2);
+    angles[i] -= col;
+  }
+  endShape();
+  
+  
 }
